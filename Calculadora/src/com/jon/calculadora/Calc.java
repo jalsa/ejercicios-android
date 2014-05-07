89c3186bf80bb62d7e3dd28dc2f9c63951280022
jalsa <jon_asdo@hotmail.com>
2014-05-07 13:28:46 +0200

    Ejercicio Calculadora operaciones añadidas.

--- Calculadora/src/com/jon/calculadora/Calc.java
+++ Calculadora/src/com/jon/calculadora/Calc.java
@@ -5,11 +5,29 @@ import android.widget.Button;
 
 public class Calc {
 	
+	private double primero, segundo;
 	private double resultado;
 	
 	public void guardarNumero(String c, View v) {
 		Button btn = (Button) v;
-		int num = Integer.parseInt((String) btn.getText());
-		resultado = Double.parseDouble(c);
+		String operacion = String.valueOf(btn.getText());
+		if (operacion == "+" || operacion == "-" || operacion == "*" || operacion == "/") {
+			primero = Double.parseDouble(c);
+		}
+		else if (operacion == "=") {
+			segundo = Double.parseDouble(c);
+			if(operacion == "+") {
+				resultado = primero + segundo;
+			}
+			else if (operacion == "-") {
+				resultado = primero - segundo;
+			}
+			else if (operacion == "*") {
+				resultado = primero * segundo;		
+						}
+			else if (operacion == "/") {
+				resultado = primero / segundo;
+			}
+		}
 	}
 }
