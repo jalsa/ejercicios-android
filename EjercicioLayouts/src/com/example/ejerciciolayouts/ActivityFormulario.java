2ac7e73ab0c98ea49f4869eb1a0816e1018c5c73
jalsa <jon_asdo@hotmail.com>
2014-05-07 10:18:25 +0200

    Ejercicio de layouts modificado y fragmentos quitados.

--- EjercicioLayouts/src/com/example/ejerciciolayouts/ActivityFormulario.java
+++ EjercicioLayouts/src/com/example/ejerciciolayouts/ActivityFormulario.java
@@ -17,11 +17,6 @@ public class ActivityFormulario extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_formulario);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
 	}
 
 	@Override
@@ -42,23 +37,6 @@ public class ActivityFormulario extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(
-					R.layout.fragment_activity_formulario, container, false);
-			return rootView;
-		}
 	}
 
 }
