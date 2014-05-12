71731e3988216725c1f4e80936e9f97ef43b2c61
jalsa <jon_asdo@hotmail.com>
2014-05-12 12:16:54 +0200

    Ejercicio con ListFragment modificado, adaptador notificado en el fragmento.

--- ToDoListConFragmentos/src/com/jon/todolistconfragmentos/FragmentoLista.java
+++ ToDoListConFragmentos/src/com/jon/todolistconfragmentos/FragmentoLista.java
@@ -1,13 +1,48 @@
 package com.jon.todolistconfragmentos;
 
+import java.util.ArrayList;
+
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 public class FragmentoLista extends ListFragment {
-	/*@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-		setListShown(true);
-	}*/
+	
+	private ArrayList<String> listado;
+	private ArrayAdapter<String> adaptador;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		listado = new ArrayList<String>();
+		adaptador = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, listado);
+		
+		setListAdapter(adaptador);
+		
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		if (savedInstanceState != null) {
+			listado.addAll(savedInstanceState.getStringArrayList("listado"));
+			adaptador.notifyDataSetChanged();
+		}
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		savedInstanceState.putStringArrayList("listado", listado);
+		super.onSaveInstanceState(savedInstanceState);
+	}
+
+	public void nuevoElemento(String valor) {
+		listado.add(valor);
+		adaptador.notifyDataSetChanged();
+	}
+	
 }
--- ToDoListConFragmentos/src/com/jon/todolistconfragmentos/MainActivity.java
+++ ToDoListConFragmentos/src/com/jon/todolistconfragmentos/MainActivity.java
@@ -1,62 +1,32 @@
 package com.jon.todolistconfragmentos;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 
 public class MainActivity extends Activity implements FragmentoInput.IActividad {
 
-	private ArrayList<String> listado;
-	private ArrayAdapter<String> adaptador;
-	private FragmentoLista fragmentoLista;
-
+	FragmentManager fragmentManager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-		listado = new ArrayList<String>();
-		adaptador = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, listado);
-
-		fragmentoLista = new FragmentoLista();
-
-//		if (savedInstanceState == null) {
-			FragmentManager fragmentManager = getFragmentManager();
-			FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-			fragmentTransaction.replace(R.id.input, new FragmentoInput());
-			fragmentTransaction.replace(R.id.lista, fragmentoLista);
+		if (savedInstanceState == null) {
+			fragmentTransaction.add(R.id.input, new FragmentoInput());
+			fragmentTransaction.add(R.id.lista, new FragmentoLista(), "list");
 			fragmentTransaction.commit();
-//		}
+		}
 
-		fragmentoLista.setListAdapter(adaptador);
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		adaptador.notifyDataSetChanged();
-	}
-
-	@Override
-	public void onRestoreInstanceState(Bundle savedInstanceState) {
-		super.onRestoreInstanceState(savedInstanceState);
-		listado.addAll(savedInstanceState.getStringArrayList("listado"));
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle savedInstanceState) {
-		savedInstanceState.putStringArrayList("listado", listado);
-		super.onSaveInstanceState(savedInstanceState);
 	}
 
 	public void nuevoElemento(String valor) {
-		listado.add(valor);
-		adaptador.notifyDataSetChanged();
+		((FragmentoLista)getFragmentManager().findFragmentByTag("list")).nuevoElemento(valor);
 	}
 
 }
