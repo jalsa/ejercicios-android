a8272ed542dccb9d3488c4e237a92b7517e1f09b
jalsa <jon_asdo@hotmail.com>
2014-05-13 11:32:55 +0200

    Ejercicio con intents, listeners movidos al onCreate().

--- EjercicioIntents/src/com/jon/ejerciciointents/MainActivity.java
+++ EjercicioIntents/src/com/jon/ejerciciointents/MainActivity.java
@@ -1,9 +1,16 @@
 package com.jon.ejerciciointents;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,22 +25,12 @@ public class MainActivity extends Activity {
 	private String valorDeVuelta;
 	private static final int CODIGO_FOTO = 1;
 	private ImageView imagen;
+	static String mCurrentPhotoPath;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		Intent intent = getIntent();
-		valorDeVuelta = intent.getStringExtra("cadena1");
-		label = (TextView)findViewById(R.id.labelMain);
-		label.setText(valorDeVuelta);
-		
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
 		
 		OnClickListener formulario = new OnClickListener() {
 			
@@ -51,11 +48,26 @@ public class MainActivity extends Activity {
 		botonForm.setOnClickListener(formulario);
 		
 		OnClickListener camara = new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
+				//Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				//startActivityForResult(intent, CODIGO_FOTO);
 				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				startActivityForResult(intent, CODIGO_FOTO);
+			    // Ensure that there's a camera activity to handle the intent
+			    if (intent.resolveActivity(getPackageManager()) != null) {
+			        // Create the File where the photo should go
+			        File photoFile = null;
+			        try {
+			            photoFile = createImageFile();
+			        } catch (IOException ex) {
+			            // Error occurred while creating the File
+			        }
+			        // Continue only if the File was successfully created
+			        if (photoFile != null) {
+			            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+			            startActivityForResult(intent, CODIGO_FOTO);
+			        }
+			    }
 			}
 		};
 		
@@ -73,6 +85,21 @@ public class MainActivity extends Activity {
 		botonContact = (Button)findViewById(R.id.botonContacto);
 		botonContact.setOnClickListener(contacto);
 		
+		Intent intent = getIntent();
+		valorDeVuelta = intent.getStringExtra("cadena1");
+		label = (TextView)findViewById(R.id.labelMain);
+		label.setText(valorDeVuelta);
+		
+	}
+	
+	public static File createImageFile() throws IOException {
+	    // Create an image file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = "JPEG_" + timeStamp + "_";
+	    File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+	    File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+	    mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+	    return image;
 	}
 	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
