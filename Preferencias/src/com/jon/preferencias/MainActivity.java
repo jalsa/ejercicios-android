6305961a52a72db317430a89bed2a62265c4d58c
jalsa <jon_asdo@hotmail.com>
2014-05-16 12:05:34 +0200

    Las preferencias se guardan y se obtienen correctamente.

--- Preferencias/src/com/jon/preferencias/MainActivity.java
+++ Preferencias/src/com/jon/preferencias/MainActivity.java
@@ -1,46 +1,43 @@
 package com.jon.preferencias;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	
 	private SharedPreferences mySharedPreferences;
 	public static final String MY_PREFS = "PREFERENCIAS";
+	public static final String VALORREFRESH = "valorRefresh";
+	public static final String VALORINTERVALO = "valorIntervalo";
+	TextView refresh, intervalo;
+	boolean boolRefresh;
+	int indiceIntervalo;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
-		}
-		
-		TextView refresh = (TextView) findViewById(R.id.valorAuto);
-		TextView intervalo = (TextView) findViewById(R.id.valorIntervalo);
-		
+		refresh = (TextView) findViewById(R.id.valorAuto);
+		intervalo = (TextView) findViewById(R.id.valorIntervalo);	
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
 		mySharedPreferences = getSharedPreferences(MY_PREFS, Activity.MODE_PRIVATE);
-		String stringRefresh = mySharedPreferences.getString("valorRefresh", "");
-		String stringIntervalo = mySharedPreferences.getString("valorIntervalo", "");
-		
-		refresh.setText(stringRefresh);
-		intervalo.setText(stringIntervalo);
+		boolRefresh = mySharedPreferences.getBoolean(VALORREFRESH, true);
+		indiceIntervalo = mySharedPreferences.getInt(VALORINTERVALO, 0);
+		refresh.setText(String.valueOf(boolRefresh));
+		intervalo.setText(String.valueOf(indiceIntervalo));
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
@@ -59,22 +56,4 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
-
 }
--- Preferencias/src/com/jon/preferencias/SettingsActivity.java
+++ Preferencias/src/com/jon/preferencias/SettingsActivity.java
@@ -9,32 +9,40 @@ import android.widget.ToggleButton;
 
 public class SettingsActivity extends Activity {
 
-	public static final String MY_PREFS = "PREFERENCIAS";
 	ToggleButton boton;
 	Spinner spinner;
 	private SharedPreferences mySharedPreferences;
+	boolean boolRefresh;
+	int indiceIntervalo;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
-
 		boton = (ToggleButton) findViewById(R.id.toggleBoton);
 		spinner = (Spinner) findViewById(R.id.spinnerIntervalo);
-		
 		Spinner spinner = (Spinner) findViewById(R.id.spinnerIntervalo);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.intervalos_array, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner.setAdapter(adapter);
-		
+		spinner.setAdapter(adapter);	
+	}
+	
+	public void onResume() {
+		super.onResume();
+		mySharedPreferences = getSharedPreferences(MainActivity.MY_PREFS, Activity.MODE_PRIVATE);
+		boolRefresh = mySharedPreferences.getBoolean(MainActivity.VALORREFRESH, true);
+		indiceIntervalo = mySharedPreferences.getInt(MainActivity.VALORINTERVALO, 0);
+		boton.setChecked(boolRefresh);
+		spinner.setSelection(indiceIntervalo);
 	}
 	
 	@Override
 	public void onPause() {
-		mySharedPreferences = getSharedPreferences(MY_PREFS, Activity.MODE_PRIVATE);
+		super.onPause();
+		mySharedPreferences = getSharedPreferences(MainActivity.MY_PREFS, Activity.MODE_PRIVATE);
 		SharedPreferences.Editor editor = mySharedPreferences.edit();
-		editor.putString("valorRefresh", (String) boton.getText());
-		editor.putString("valorIntervalo", (String) spinner.getSelectedItem());
+		editor.putBoolean(MainActivity.VALORREFRESH,boton.isChecked());
+		editor.putInt(MainActivity.VALORINTERVALO, spinner.getSelectedItemPosition());
 		editor.apply();
 	}
 }
