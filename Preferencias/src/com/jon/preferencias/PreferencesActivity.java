dc4449e06c0982888b1b251a97b30acf457cac2b
jalsa <jon_asdo@hotmail.com>
2014-05-19 12:17:52 +0200

    Pantalla con el header quitada porque solo hay un header.

--- Preferencias/src/com/jon/preferencias/PreferencesActivity.java
+++ Preferencias/src/com/jon/preferencias/PreferencesActivity.java
@@ -3,10 +3,13 @@ package com.jon.preferencias;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
-public class PreferencesActivity extends PreferenceActivity {
+/*public class PreferencesActivity extends PreferenceActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +20,21 @@ public class PreferencesActivity extends PreferenceActivity {
 		loadHeadersFromResource(R.xml.userpreferenceheaders, target);
 	}
 
-}
+}*/
 
-/*public class PreferencesActivity extends Activity {
+public class PreferencesActivity extends Activity implements OnSharedPreferenceChangeListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getFragmentManager().beginTransaction().replace(R.id.container, new FragmentoPreferencia()).commit();
+		getFragmentManager().beginTransaction().replace(android.R.id.content, new FragmentoPreferencia()).commit();
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		prefs.registerOnSharedPreferenceChangeListener(this);
 	}
 
-}*/
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences arg0, String arg1) {
+		System.out.println("HOLA");
+	}
+
+}
