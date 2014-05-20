8d9961a479d3f230499d50d86013db3a33b833c2
jalsa <jon_asdo@hotmail.com>
2014-05-20 12:23:42 +0200

    Se insertan, actualizan y borran terremotos correctamente.

--- Earthquakes/src/com/jon/earthquakes/MainActivity.java
+++ Earthquakes/src/com/jon/earthquakes/MainActivity.java
@@ -16,5 +16,8 @@ public class MainActivity extends Activity {
 		dbOpenHelper = new DBOpenHelper(this, DBOpenHelper.DATABASE_NAME, null, DBOpenHelper.DATABASE_VERSION);
 		db = EarthquakeDB.open(dbOpenHelper);
 		EarthquakeDB.insert(db, "50", "Errenteria", "12:00", "Menudo terremoto!", Float.valueOf("7.9"), Float.valueOf("43.312527"), Float.valueOf("-1.898613"), "http://es.wikipedia.org/wiki/Renter%C3%ADa");
+		EarthquakeDB.insert(db, "60", "Bilbao", "13:00", "El terremoto m‡s grande del mundo!", Float.valueOf("10"), Float.valueOf("43.256944"), Float.valueOf("-2.923611"), "http://es.wikipedia.org/wiki/Bilbao");
+		EarthquakeDB.update(db, DBOpenHelper.PLACE_COLUMN, "Donostia", DBOpenHelper.ID_COLUMN + " = ?", new String[]{"50"});
+		EarthquakeDB.delete(db, DBOpenHelper.ID_COLUMN + " = ?", new String[]{"50"});
 	}
 }
