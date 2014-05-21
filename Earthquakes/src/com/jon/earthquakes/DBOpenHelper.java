9561e89a69eb7aff7acb51844c2b8a302ffdaf46
jalsa <jon_asdo@hotmail.com>
2014-05-21 11:55:18 +0200

    Nombres de las columnas públicas.

--- Earthquakes/src/com/jon/earthquakes/DBOpenHelper.java
+++ Earthquakes/src/com/jon/earthquakes/DBOpenHelper.java
@@ -7,21 +7,21 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBOpenHelper extends SQLiteOpenHelper {
 	
-	protected static final String DATABASE_NAME = "earthquakes.db";
-	protected static final String DATABASE_TABLE = "earthquakes";
-	protected static final int DATABASE_VERSION = 1;
+	public static final String DATABASE_NAME = "earthquakes.db";
+	public static final String DATABASE_TABLE = "earthquakes";
+	public static final int DATABASE_VERSION = 1;
 	
-	protected static final String ID_COLUMN = "_id";
-	protected static final String ID_STR_COLUMN = "id_str";
-	protected static final String PLACE_COLUMN = "place";
-	protected static final String TIME_COLUMN = "time";
-	protected static final String DETAIL_COLUMN = "detail";
-	protected static final String MAGNITUDE_COLUMN = "magnitude";
-	protected static final String LAT_COLUMN = "lat";
-	protected static final String LONG_COLUMN = "long";
-	protected static final String URL_COLUMN = "url";
-	protected static final String CREATED_AT_COLUMN = "created_at";
-	protected static final String UPDATED_AT_COLUMN = "updated_at";
+	public static final String ID_COLUMN = "_id";
+	public static final String ID_STR_COLUMN = "id_str";
+	public static final String PLACE_COLUMN = "place";
+	public static final String TIME_COLUMN = "time";
+	public static final String DETAIL_COLUMN = "detail";
+	public static final String MAGNITUDE_COLUMN = "magnitude";
+	public static final String LAT_COLUMN = "lat";
+	public static final String LONG_COLUMN = "long";
+	public static final String URL_COLUMN = "url";
+	public static final String CREATED_AT_COLUMN = "created_at";
+	public static final String UPDATED_AT_COLUMN = "updated_at";
 	
 	private static final String DATABASE_CREATE = "CREATE TABLE IF NOT EXISTS " + DATABASE_TABLE + " (" + ID_COLUMN + " INTEGER PRIMARY KEY AUTOINCREMENT, "
 			+ ID_STR_COLUMN + " TEXT UNIQUE, " + PLACE_COLUMN + " TEXT, " + TIME_COLUMN + " DATETIME, " + DETAIL_COLUMN + " TEXT, " + MAGNITUDE_COLUMN + " REAL, " + LAT_COLUMN + " REAL, "
