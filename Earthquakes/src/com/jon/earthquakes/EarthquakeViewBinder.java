78ade694b91997fbd62d8906e6c844ab44a3dc13
jalsa <jon_asdo@hotmail.com>
2014-05-26 13:30:48 +0200

    Se muestra una lista personalizada con colores para las magnitudes.

--- Earthquakes/src/com/jon/earthquakes/EarthquakeViewBinder.java
+++ Earthquakes/src/com/jon/earthquakes/EarthquakeViewBinder.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 import android.database.Cursor;
+import android.graphics.Color;
 import android.view.View;
 import android.widget.SimpleCursorAdapter.ViewBinder;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ public class EarthquakeViewBinder implements ViewBinder {
 	@Override
 	public boolean setViewValue(View view, Cursor c, int idx) {
 		int time_idx = c.getColumnIndex(MyContentProvider.TIME_COLUMN);
+		int mag_idx = c.getColumnIndex(MyContentProvider.MAGNITUDE_COLUMN);
 		if (time_idx == idx) {
 			SimpleDateFormat s = new SimpleDateFormat("EEE, d MM yyyy HH:mm::ss aaa", Locale.ENGLISH);
 			String dateStr = s.format(c.getLong(time_idx));
@@ -20,6 +22,18 @@ public class EarthquakeViewBinder implements ViewBinder {
 			((TextView)view.findViewById(R.id.textoHora)).setText(dateStr);
 			
 			return true;
+		}
+		if (mag_idx == idx) {
+			float mag = c.getFloat(mag_idx);
+			if (mag < 1.0) {
+				((TextView)view.findViewById(R.id.textoMagnitud)).setTextColor(Color.GREEN);
+			}
+			else if (mag >= 1.0 && mag < 4.0) {
+				((TextView)view.findViewById(R.id.textoMagnitud)).setTextColor(Color.BLUE);
+			}
+			else if (mag >= 4.0) {
+				((TextView)view.findViewById(R.id.textoMagnitud)).setTextColor(Color.RED);
+			}
 		}
 		return false;
 	}
