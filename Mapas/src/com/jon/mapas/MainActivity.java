d8ccf72e949aa674b5017ec709edd89d40aac2a0
jalsa <jon_asdo@hotmail.com>
2014-05-29 13:33:17 +0200

    Añadidos servicios de Google Play.

--- Mapas/src/com/jon/mapas/MainActivity.java
+++ Mapas/src/com/jon/mapas/MainActivity.java
@@ -22,7 +22,8 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
+		setContentView(R.layout.map_layout);
 		
 		textoLatitud = (TextView) findViewById(R.id.textoLatitud);
 		textoLongitud = (TextView) findViewById(R.id.textoLongitud);
